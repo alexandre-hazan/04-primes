a30236a8f1191f799e1839e45f06d3dbeba69e9a
alexandre-hazan <alexandre.hazan@edu.esiee.fr>
2024-11-08 13:48:40 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+""" import de la fonction sqrt """
 from math import sqrt
+
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """methode qui renvoie True si le nombre est premier """
+    if p==1:
+        return False
+    for i in range (2,(int)(sqrt(p)+1)):
+        if p %i==0:
+            return False
+    return True
 
-    # votre code ici
-
-    pass
 
 #### Fonction principale
 
 
 def main():
+    """main qui s'occupe de tester la fonction créée précedement """
 
     # vos appels à la fonction secondaire ici
-
+    isprime(33)
+    isprime(43)
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
